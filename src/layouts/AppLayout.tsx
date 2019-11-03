6d96c81991fe00849691c1bf391a53489e43fd12
sschandi <sukhdeep.29@live.ca>
2019-11-03 12:09:16 -0800

    feat: html fills 100% height, appLayout styling

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 const appLayout = ({ children }) => {
   return (
-    <>
+    <div className="app-layout" style={styles}>
       {children}
-    </>
+    </div>
   )
+}
+
+const styles = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
 }
 
 export default appLayout
