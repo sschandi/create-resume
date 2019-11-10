7362e9c362560e4b0adb97d0c9b91d93d9b3fa7e
sschandi <sukhdeep.29@live.ca>
2019-11-10 12:26:45 -0800

    render content options

--- src/components/app/SelectContent.tsx
+++ src/components/app/SelectContent.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../contexts/AppContext'
+
+const selectOptions = [
+  { type: 'text', title: 'Summary' },
+  { type: 'education', title: 'Education' },
+  { type: 'skill', title: 'Skills' },
+  { type: 'list', title: 'Experience' },
+  { type: 'list', title: 'Volunteer' },
+  { type: 'reference', title: 'References' }
+]
+
+const customSelectOptions = [
+  { type: 'list', title: 'List' },
+  { type: 'text', title: 'Text' },
+  { type: 'skill', title: 'Skill' },
+]
 
 const SelectContent = () => {
+  const { } = useContext(AppContext)
   return (
     <div>
-      <h1>This is Select Content Component</h1>
+      <h1>Select Content</h1>
+      <div>
+        {selectOptions.map((option, index) => {
+          return <button key={index}>{option.title}</button>
+        })}
+      </div>
+      <div>
+        <p>Custom Options</p>
+        {customSelectOptions.map((option, index) => {
+          return <button key={index}>{option.title}</button>
+        })}
+      </div>
     </div>
   )
 }
