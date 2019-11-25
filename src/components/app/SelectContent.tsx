d1f96b87ffaeff05d94d255a645f22e045b8ad59
sschandi <sukhdeep.29@live.ca>
2019-11-24 21:32:16 -0800

    feat: init default content values to match types

--- src/components/app/SelectContent.tsx
+++ src/components/app/SelectContent.tsx
@@ -3,18 +3,95 @@ import { AppContext } from '../../contexts/AppContext'
 import { SectionTypes } from './ResumeTypes'
 
 const selectOptions = [
-  { type: SectionTypes.TEXT, title: 'Summary' },
-  { type: SectionTypes.EDUCATION, title: 'Education' },
-  { type: SectionTypes.SKILL, title: 'Skills' },
-  { type: SectionTypes.LIST, title: 'Experience' },
-  { type: SectionTypes.LIST, title: 'Volunteer' },
-  { type: SectionTypes.REFERENCE, title: 'References' }
+  {
+    type: SectionTypes.TEXT,
+    title: 'Summary',
+    elements: [
+      { elements: ['Summary Here'] }
+    ]
+  },
+  {
+    type: SectionTypes.EDUCATION,
+    title: 'Education',
+    elements: [
+      { degree: '', program: '', university: '', date: '' }
+    ]
+  },
+  {
+    type: SectionTypes.SKILL,
+    title: 'Skills',
+    elements: [
+      { name: 'Your Skill', levels: [ true, true, true, true, false ]},
+      { name: 'Your Skill', levels: [ true, true, true, false, false ]}
+    ]
+  },
+  {
+    type: SectionTypes.LIST,
+    title: 'Experience',
+    elements: [
+      {
+        title: 'Reginal Manager', extra: '', elements: [
+          'Directed staff.'
+        ]
+      },
+      {
+        title: 'Reginal Manager', extra: '', elements: [
+          'Directed staff.'
+        ]
+      }
+    ]
+  },
+  {
+    type: SectionTypes.LIST,
+    title: 'Volunteer',
+    elements: [
+      {
+        title: 'Reginal Manager', extra: '', elements: [
+          'Directed staff.'
+        ]
+      },
+      {
+        title: 'Reginal Manager', extra: '', elements: [
+          'Directed staff.'
+        ]
+      }
+    ]
+  },
+  {
+    type: SectionTypes.REFERENCE,
+    title: 'References',
+    elements: [
+      {
+        name: 'John Smith',
+        occupation: 'Sales',
+        company: 'Washington',
+        companyAddress: '123',
+        phone: '(555) 123 - 4312',
+        email: 'jimmy@company.com'
+      }
+    ]
+  }
 ]
 
 const customSelectOptions = [
-  { type: SectionTypes.LIST, title: 'List' },
-  { type: SectionTypes.TEXT, title: 'Text' },
-  { type: SectionTypes.SKILL, title: 'Skill' },
+  {
+    type: SectionTypes.LIST,
+    title: 'List',
+    elements: [{ title: '', extra: '', elements: [''] }]
+  },
+  {
+    type: SectionTypes.TEXT,
+    title: 'Text',
+    elements: [{ elements: [''] }]
+  },
+  {
+    type: SectionTypes.SKILL,
+    title: 'Skill',
+    elements: [
+      { name: '', levels: [ true, true, true, true, false ]},
+      { name: 'Your Skill', levels: [ true, true, true, false, false ]}
+    ]
+  },
 ]
 
 const SelectContent = () => {
@@ -25,13 +102,27 @@ const SelectContent = () => {
       <h1>Select Content</h1>
       <div>
         {selectOptions.map((option, index) => {
-          return <button key={index} onClick={() => addSection({ type: option.type, title: option.title, elements: []})}>{option.title}</button>
+          return (
+            <button
+              key={index}
+              onClick={() => addSection({ type: option.type, title: option.title, elements: option.elements })}
+            >
+              {option.title}
+            </button>
+          )
         })}
       </div>
       <div>
         <p>Custom Options</p>
         {customSelectOptions.map((option, index) => {
-          return <button key={index} onClick={() => addSection({ type: option.type, title: option.title, elements: []})}>{option.title}</button>
+          return (
+            <button
+              key={index}
+              onClick={() => addSection({ type: option.type, title: option.title, elements: option.elements })}
+            >
+              {option.title}
+            </button>
+          )
         })}
       </div>
       {sections.map((section, index) => {
