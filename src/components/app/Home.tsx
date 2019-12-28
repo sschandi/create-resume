a4a9ae0b45437da3394cdab4aa6e48d8b2f1f638
sschandi <sukhdeep.29@live.ca>
2019-12-27 23:31:02 -0800

    feat: app home styling updates

--- src/components/app/Home.tsx
+++ src/components/app/Home.tsx
@@ -5,9 +5,12 @@ const Home = (props) => {
   return (
     <div id="home" className="component-container">
       <h1>Create Resume</h1>
-      <p>Simple resume maker where you control your data.</p>
+      <p className="subtitle">Simple resume maker where you control your data.</p>
+      <button className="btn btn-primary" onClick={() => props.next()}>Get Started</button>
       <div className="home__actions">
-        <button onClick={() => props.next()}>Get Started</button>
+        <h2>Been</h2>
+        <h2>Here Before?</h2>
+        <p>Continue working on a resume:</p>
         <Uploader />
       </div>
     </div>
