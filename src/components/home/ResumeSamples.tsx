79865df072adbd4cb216ed98f77981caf0e0c5da
sschandi <sukhdeep.29@live.ca>
2020-01-03 21:05:01 -0800

    chore(cleanup): remove old styles from js

--- src/components/home/ResumeSamples.tsx
+++ src/components/home/ResumeSamples.tsx
@@ -4,12 +4,6 @@ import CluoImg from '../../images/home/cluo-sample.png'
 import CogitoImg from '../../images/home/cogito-sample.png'
 import TeresImg from '../../images/home/teres-sample.png'
 
-const imageStyles: CSSProperties = {
-  position: 'absolute',
-  height: '100%',
-  boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)'
-}
-
 const ResumeSamples = () => {
   return (
     <div className="resume-samples">
