dafbc710943bada6174c7dc03cd197353d28d1d8
sschandi <sukhdeep.29@live.ca>
2020-01-18 22:34:43 -0800

    feat: nprogress gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,15 @@ module.exports = {
           include: /\.inline\.svg$/
         }
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#39648A`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
