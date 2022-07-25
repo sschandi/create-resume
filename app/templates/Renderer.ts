8788cb3d26e159d9067f5e5731e83143b2815935
sschandi <sukhdeep.29@live.ca>
2022-07-25 15:18:59 -0700

    feat: vfs_fonts v2, createPdf works

--- app/templates/Renderer.ts
+++ app/templates/Renderer.ts
@@ -1,57 +1,39 @@
+// VFS fonts were rebuilt using instructions on https://pdfmake.github.io/docs/0.1/fonts/custom-fonts-client-side/vfs/
+// Once the fonts were built I changed the top line of the created file from
+// 'this.pdfMake = this.pdfMake || {}; this.pdfMake.vfs =' to 'export const vfs ='
+// so we wouldn't run into an error where 'this.pdfMake' is undefined
+// All fonts available in vfs_fonts-2 are located in ./fonts here as well
+// Currently Italic fonts re-use normal fonts but they are included in the vfs_fonts-2 build
+// but no template uses italics right now as well.
 const pdfMake = require('pdfmake/build/pdfmake')
-// const pdfFonts = require('./vfs_fonts')
-// pdfMake.vfs = pdfFonts
-// TODO: trying to not use the ./vfs_fonts (doesn't work) instead trying to add all fonts locally
-// but didn't get all fonts yet
+const pdfFonts = require('./vfs_fonts-2')
+pdfMake.vfs = pdfFonts.vfs
+
 pdfMake.fonts = {
   Roboto: {
-    normal: './fonts/Roboto-Regular.ttf',
-    bold: './fonts/Roboto-Medium.ttf',
-    italics: './fonts/Roboto-Regular.ttf',
-    bolditalics: './fonts/Roboto-Regular.ttf'
+    normal: 'Roboto-Regular.ttf',
+    bold: 'Roboto-Medium.ttf',
+    italics: 'Roboto-Regular.ttf',
+    bolditalics: 'Roboto-Regular.ttf'
   },
   OpenSans: {
-    normal: './fonts/Roboto-Regular.ttf',
-    bold: './fonts/Roboto-Medium.ttf',
-    italics: './fonts/Roboto-Regular.ttf',
-    bolditalics: './fonts/Roboto-Regular.ttf'
+    normal: 'OpenSans-Regular.ttf',
+    bold: 'OpenSans-Bold.ttf',
+    italics: 'OpenSans-Regular.ttf',
+    bolditalics: 'OpenSans-Regular.ttf'
   },
   CrimsonText: {
-    normal: './fonts/Roboto-Regular.ttf',
-    bold: './fonts/Roboto-Medium.ttf',
-    italics: './fonts/Roboto-Regular.ttf',
-    bolditalics: './fonts/Roboto-Regular.ttf'
+    normal: 'CrimsonText-Regular.ttf',
+    bold: 'CrimsonText-Bold.ttf',
+    italics: 'CrimsonText-Regular.ttf',
+    bolditalics: 'CrimsonText-Regular.ttf'
   },
   Quicksand: {
-    normal: './fonts/Roboto-Regular.ttf',
-    bold: './fonts/Roboto-Medium.ttf',
-    italics: './fonts/Roboto-Regular.ttf',
-    bolditalics: './fonts/Roboto-Regular.ttf'
+    normal: 'Quicksand-Regular.ttf',
+    bold: 'Quicksand-Medium.ttf',
+    italics: 'Quicksand-Regular.ttf',
+    bolditalics: 'Quicksand-Regular.ttf'
   }
-  // Roboto: {
-  //   normal: 'Roboto-Regular.ttf',
-  //   bold: 'Roboto-Medium.ttf',
-  //   italics: 'Roboto-Regular.ttf',
-  //   bolditalics: 'Roboto-Regular.ttf'
-  // },
-  // OpenSans: {
-  //   normal: 'OpenSans-Regular.ttf',
-  //   bold: 'OpenSans-Bold.ttf',
-  //   italics: 'OpenSans-Regular.ttf',
-  //   bolditalics: 'OpenSans-Regular.ttf'
-  // },
-  // CrimsonText: {
-  //   normal: 'CrimsonText-Regular.ttf',
-  //   bold: 'CrimsonText-Bold.ttf',
-  //   italics: 'CrimsonText-Regular.ttf',
-  //   bolditalics: 'CrimsonText-Regular.ttf'
-  // },
-  // Quicksand: {
-  //   normal: 'Quicksand-Regular.ttf',
-  //   bold: 'Quicksand-Medium.ttf',
-  //   italics: 'Quicksand-Regular.ttf',
-  //   bolditalics: 'Quicksand-Regular.ttf'
-  // }
 }
 import { Colors } from './Interfaces'
 import Teres from './Teres'
