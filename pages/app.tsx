0dfc76a4c706d3064ddb2eb53308555e7cc9d854
sschandi <sukhdeep.29@live.ca>
2022-07-30 13:00:42 -0700

    feat: remove react-spring parallax app navigation for simplified page switch

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import React, { useState, useEffect, ReactElement } from 'react'
 import AppContextProvider from '../contexts/AppContext'
 import Home from '../app/Home'
 import Header from '../app/Header'
@@ -10,10 +9,6 @@ import Navigation from '../app/Navigation'
 import AppLayout from '../layouts/AppLayout'
 import SEO from '../components/seo'
 
-import CluoSample from '../images/home/cluo-sample.png'
-import TeresSample from '../images/home/teres-sample.png'
-import CogitoSample from '../images/home/cogito-sample.png'
-
 enum AppComponents {
   Home = 0,
   Header,
@@ -23,18 +18,22 @@ enum AppComponents {
 
 const App: React.FC = () => {
   const [currentComponent, setCurrentComponent] = useState<AppComponents>(AppComponents.Home)
-  const parallax: any = useRef(null)
-  // Parallax Components Props
-  const componentProps = {
-    speed: 0,
-    // Style is now set directly on the component because ts errored when set here
-    // style: { overflowY: 'auto' },
-    // onScroll: (e: any) => e.stopPropagation()
+
+  const appPage = (prop: AppComponents): ReactElement<any, any> => {
+    switch (prop) {
+      case AppComponents.Home:
+        return <Home next={goNext} />
+      case AppComponents.Header:
+        return <Header />
+      case AppComponents.Content:
+        return <Content active={currentComponent === AppComponents.Content} />
+      case AppComponents.Design:
+        return <Design active={currentComponent === AppComponents.Design} />
+    }
   }
 
   const go = (to: AppComponents) => {
     setCurrentComponent(to)
-    parallax.current.scrollTo(to)
   }
 
   const goBack = () => {
@@ -47,7 +46,6 @@ const App: React.FC = () => {
   }
 
   useEffect(() => {
-    console.log(currentComponent)
     go(currentComponent)
   }, [currentComponent])
 
@@ -66,47 +64,9 @@ const App: React.FC = () => {
     <AppLayout>
       <SEO title="App" />
       <AppContextProvider>
-        <Parallax pages={5} ref={parallax} horizontal={true} enabled={false}>
-          {/* BG Designs */}
-          <ParallaxLayer offset={0} factor={1} speed={1}>
-            <div className="app-bg">
-              <img src={TeresSample.src} alt="" className="parallax-img parallax-img__1" />
-              <img src={CogitoSample.src} alt="" className="parallax-img parallax-img__2" />
-              <img src={CluoSample.src} alt="" className="parallax-img parallax-img__3" />
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer offset={0.25} factor={1.25} speed={.4}>
-            <div className="app-bg__triangle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 205.05208 132.29167"
-                width="100%"
-                height="100%"
-                preserveAspectRatio="none">
-                <g
-                  id="layer1">
-                  <path
-                    d="M 139.39734,3.6290322e-6 -2.375e-6,132.29166 H 139.39734 205.05209 V 3.6290322e-6 Z"
-                    style={{ fill: '#2d9cca' }}
-                    id="rect833" />
-                </g>
-              </svg>
-            </div>
-          </ParallaxLayer>
-          {/* Main components */}
-          <ParallaxLayer offset={0} {...componentProps} style={{ overflowY: 'auto' }}>
-            <Home next={goNext} />
-          </ParallaxLayer>
-          <ParallaxLayer offset={1} {...componentProps} style={{ overflowY: 'auto' }}>
-            <Header />
-          </ParallaxLayer>
-          <ParallaxLayer offset={2} {...componentProps} style={{ overflowY: 'auto' }}>
-            <Content active={currentComponent === AppComponents.Content} />
-          </ParallaxLayer>
-          <ParallaxLayer offset={3} {...componentProps} style={{ overflowY: 'auto' }}>
-            <Design active={currentComponent === AppComponents.Design} />
-          </ParallaxLayer>
-        </Parallax>
+        <div className="app-page">
+          {appPage(currentComponent)}
+        </div>
       </AppContextProvider>
       <Navigation current={currentComponent} prev={goBack} next={goNext} go={go} />
     </AppLayout>
