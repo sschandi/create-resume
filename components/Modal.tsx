cb9f4d81b5b388f48ed0d481de9694e707a45c4a
sschandi <sukhdeep.29@live.ca>
2022-07-30 13:01:24 -0700

    fix: fix react portal error with next server side rendering

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { animated, useSpring } from '@react-spring/web'
 
@@ -12,9 +12,15 @@ interface Props {
 const Modal: React.FC<Props> = ({ show, title, close, children }) => {
   const wrapper = useSpring({ opacity: show ? 1 : 0, visibility: show ? 'visible' : 'hidden' })
   const container = useSpring({ transform: show ? 'scale(1)' : 'scale(0)' })
+  const [mount, setMount]= useState(false)
+
+  // Workaround for Nextjs 'document.body is undefined' during server side compilation of portal
+  useEffect(() => {
+    setMount(true)
+  }, [])
 
   return (
-    ReactDOM.createPortal(
+    mount ? ReactDOM.createPortal(
       // @ts-ignore
       <animated.div style={wrapper} className="modal__wrapper">
         <animated.div style={container} className="modal__container text--center">
@@ -27,7 +33,7 @@ const Modal: React.FC<Props> = ({ show, title, close, children }) => {
           </div>
         </animated.div>
       </animated.div>
-    , document.body)
+    , document.body) : <div></div>
   )
 }
 
