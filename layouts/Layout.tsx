f670559dfd15ebfa0071295a8103d19bf6807966
sschandi <sukhdeep.29@live.ca>
2022-08-16 09:16:34 -0700

    feat: 404 page

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Logo from '../images/cr-logo.svg'
 
-const Layout: React.FC<{ fluid?: boolean, children: JSX.Element[] }> = ({ fluid = false, children }) => {
+const Layout: React.FC<{ fluid?: boolean, children: JSX.Element | JSX.Element[] }> = ({ fluid = false, children }) => {
   return (
     <>
       <div id="navigation">
