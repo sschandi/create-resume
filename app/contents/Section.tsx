31c95fd38898896e965501b9929d85c35866e5a8
sschandi <sukhdeep.29@live.ca>
2023-04-02 11:51:13 -0700

    feat: update app icon buttons

--- app/contents/Section.tsx
+++ app/contents/Section.tsx
@@ -53,14 +53,14 @@ const Section: React.FC<Props> = ({ section, index }) => {
           />
         </div>
         <div className="content__title--actions">
-          <button className="btn btn-icon icon__delete" style={{ marginRight: '0.5rem' }} onClick={() => deleteSection(index)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line>
+          <button className="btn__app-icon delete" style={{ marginRight: '0.5rem' }} onClick={() => deleteSection(index)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="24" width="24">
+              <path xmlns="http://www.w3.org/2000/svg" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="currentColor"></path>
             </svg>
           </button>
           <button
             disabled={index === 0}
-            className="btn btn-icon"
+            className="btn__app-icon"
             onClick={() => reorderSection(index, index - 1)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -69,7 +69,7 @@ const Section: React.FC<Props> = ({ section, index }) => {
           </button>
           <button
             disabled={index === sections.length - 1}
-            className="btn btn-icon"
+            className="btn__app-icon"
             onClick={() => reorderSection(index, index + 1)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
