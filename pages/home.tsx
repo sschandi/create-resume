5efb7c1e9bf8da5dba6afffb381c5ffa0ddb787c
sschandi <sukhdeep.29@live.ca>
2023-04-01 17:46:10 -0700

    feat: home page detail sections

--- pages/home.tsx
+++ pages/home.tsx
@@ -24,27 +24,55 @@ const Home: NextPage = () => {
         </div>
         <div className={styles.action}>
           <Link href="/app">
-            <a className="btn btn-link btn-primary btn-lg">Get Started for Free</a>
+            <a className="btn btn-link btn-primary btn-lg">Create Your Resume</a>
           </Link>
-          {/* <div className={styles.or}>
-            <span>Or</span>
-          </div> */}
           <Link href={{ pathname: '/app', query: { continue: true }}}>
             <a className="btn btn-link btn-secondary">Continue Working</a>
           </Link>
-          {/* Removed in favour of redirecting users to app with continue query */}
-          {/* <div className={styles.dropperWrapper}>
-            <div className={styles.dropper}>
-              Continue working on your resume.<br />
-              Click to open your PDF.
-            </div>
-          </div> */}
         </div>
         <div className={styles.spacer} />
         <div className={styles.samples}>
           <img src={CluoImg.src} alt="" className={styles.sampleImg + ' ' + styles.sampleOne} />
           <img src={CogitoImg.src} alt="" className={styles.sampleImg}  />
           <img src={TeresImg.src} alt="" className={styles.sampleImg + ' ' + styles.sampleThree}  />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className={styles.createHeader}>Create Your Perfect Resume</h1>
+        <div className={styles.create}>
+          <div className={styles.createStep}>
+            <div className={styles.createNumber}>1.</div>
+            <h2>Create</h2>
+            <p>Add your content and tweak the formatting to personalize your resume message.</p>
+          </div>
+          <div className={styles.createStep}>
+            <div className={styles.createNumber}>2.</div>
+            <h2>Design</h2>
+            <p>Choose from a selection of professional templates, then customize your colors to match your style.</p>
+          </div>
+          <div className={styles.createStep}>
+            <div className={styles.createNumber}>3.</div>
+            <h2>Export</h2>
+            <p>Generate a PDF resume that&apos;s ready to download, print, or email to potential employers.</p>
+          </div>
+          <div className={styles.createStep}>
+            <div className={styles.createNumber}>4.</div>
+            <h2>Resume</h2>
+            <p>Need to update your resume later? No problem! Simply re-upload your file to pick up where you left off, with all your changes intact.</p>
+          </div>
+        </div>
+      </section>
+      <section className="container">
+        <div className={styles.privacy}>
+          <h1>Your Privacy <span>Preserved</span></h1>
+          <p>
+            Unlike many other resume-building tools, Create Resume works entirely client-side -
+            that means there&apos;s no external data processing or servers involved. Instead, everything happens right in your browser.
+            This approach ensures that <strong>your data is never seen, sent, or stored</strong> by us or anybody else.
+          </p>
+          <p>
+            As an open-source project, all of the code is available in <a href="https://github.com/sschandi/create-resume" rel="noreferrer" target="_blank" className="link">our repository.</a>
+          </p>
         </div>
       </section>
     </Layout>
