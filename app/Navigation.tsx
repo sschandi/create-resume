7273ead5d0054cf7d2c951a2e9f825c14f4dca28
sschandi <sukhdeep.29@live.ca>
2023-04-02 13:38:18 -0700

    feat: remove autofocus on header, lg screen next btn fixed bottom right

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -100,14 +100,25 @@ const Navigation: React.FC<Props> = ({ current, prev, next, go }) => {
         </div>
       </div>
       {!isMobile &&
-        <div className="app-nav__additional">
-          <div className="app-nav__uploader">
-            <Uploader next={() => {}} btnClasses="btn btn-secondary" btnText="Edit Existing Resume" />
+        <>
+          <div className="app-nav__additional">
+            <div className="app-nav__uploader">
+              <Uploader next={() => {}} btnClasses="btn btn-secondary" btnText="Edit Existing Resume" />
+            </div>
+            <div className="app-preview">
+              <Preview />
+            </div>
           </div>
-          <div className="app-preview">
-            <Preview />
+          {/* On large screens, next button is also fixed to the bottom right */}
+          <div className="app-nav__fixed-next">
+            <button disabled={current === AppComponents.Design} className="btn btn-primary" onClick={() => next()}>
+              <span>Next</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="18" height="18">
+                <path xmlns="http://www.w3.org/2000/svg" d="M9.29289 18.7071C8.90237 18.3166 8.90237 17.6834 9.29289 17.2929L14.5858 12L9.29289 6.70711C8.90237 6.31658 8.90237 5.68342 9.29289 5.29289C9.68342 4.90237 10.3166 4.90237 10.7071 5.29289L16.7071 11.2929C17.0976 11.6834 17.0976 12.3166 16.7071 12.7071L10.7071 18.7071C10.3166 19.0976 9.68342 19.0976 9.29289 18.7071Z" fill="currentColor"></path>
+              </svg>
+            </button>
           </div>
-        </div>
+        </>
       }
     </div>
   )
